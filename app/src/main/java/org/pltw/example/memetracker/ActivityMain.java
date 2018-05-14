6e223c67909daa06e7a45ab54ab67ec5b0ce0cc1
hunterhutchinson <31930550+hunterhutchinson@users.noreply.github.com>
2018-05-14 10:32:49 -0600

    Deleted stuff

--- app/src/main/java/org/pltw/example/memetracker/ActivityMain.java
+++ app/src/main/java/org/pltw/example/memetracker/ActivityMain.java
@@ -84,10 +84,6 @@ public class ActivityMain extends AppCompatActivity
 
         } else if (id == R.id.nav_garbage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
