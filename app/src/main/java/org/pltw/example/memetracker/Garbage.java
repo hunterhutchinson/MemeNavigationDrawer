a716d50ff92ab88943bfd59bd5985fbd0d276afd
20eskjev <20eskjev@mcps.k12.mt.us>
2018-05-16 10:13:40 -0600

    Declared rating for garbage memes, generated setter and getter

--- app/src/main/java/org/pltw/example/memetracker/Garbage.java
+++ app/src/main/java/org/pltw/example/memetracker/Garbage.java
@@ -1,4 +1,13 @@
 package org.pltw.example.memetracker;
 
 public class Garbage {
+    public int garbageRating;
+
+    public int getGarbage() {
+        return garbageRating;
+    }
+
+    public void setGarbage(int garbageRating) {
+        this.garbageRating = garbageRating;
+    }
 }
