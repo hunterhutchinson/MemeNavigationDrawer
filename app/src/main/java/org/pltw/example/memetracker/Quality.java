3858a7dd12eac36a4f3eec7f16a28d0596e7cccc
Joshua Giguere <18gigjos@student.mcpsmt.org>
2018-05-16 10:21:05 -0600

    more quality.java stuff

--- app/src/main/java/org/pltw/example/memetracker/Quality.java
+++ app/src/main/java/org/pltw/example/memetracker/Quality.java
@@ -3,6 +3,10 @@ package org.pltw.example.memetracker;
 public class Quality {
     public int qualityRating;
 
+    public Quality(int qualityRating) {
+        this.qualityRating = qualityRating;
+    }
+
     public int getQuality() {
         return qualityRating;
     }
