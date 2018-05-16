e3aaf0bb0fa470257500c3ae73d517670827cd31
20eskjev <20eskjev@mcps.k12.mt.us>
2018-05-16 10:19:37 -0600

    garbage constructor

--- app/src/main/java/org/pltw/example/memetracker/Garbage.java
+++ app/src/main/java/org/pltw/example/memetracker/Garbage.java
@@ -3,6 +3,10 @@ package org.pltw.example.memetracker;
 public class Garbage {
     public int garbageRating;
 
+    public Garbage(int garbageRating) {
+        this.garbageRating = garbageRating;
+    }
+
     public int getGarbage() {
         return garbageRating;
     }
