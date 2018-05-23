efacf65c849e31eff129462f53cd9608c3093903
hunterhutchinson <31930550+hunterhutchinson@users.noreply.github.com>
2018-05-23 10:10:53 -0600

    Added ImageViews to Meme_fragment

--- app/src/main/java/org/pltw/example/memetracker/Meme_Fragment.java
+++ app/src/main/java/org/pltw/example/memetracker/Meme_Fragment.java
@@ -7,10 +7,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Meme_Fragment extends ListFragment {
 
     private Meme mMeme;
+    private ImageView mSpongeBobImage1;
+    private ImageView mSpongeBobImage2;
+    private ImageView mPhotoshopImage1;
+    private ImageView mPhotoshopImage2;
+    private ImageView mJoshImage1;
+    private ImageView mOverwatchImage1;
+    private ImageView mOverwatchImage2;
+    private ImageView mFortniteImage1;
+    private ImageView mFortniteImage2;
+    private ImageView mDestinyImage1;
+    private ImageView mDestinyImage2;
 
     public Meme_Fragment() {
         mMeme = new Meme();
@@ -20,6 +33,29 @@ public class Meme_Fragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.meme_fragment, container, false);
+
+        mSpongeBobImage1 = (ImageView) rootView.findViewById(R.id.spongebob_1);
+        mSpongeBobImage2 = (ImageView) rootView.findViewById(R.id.spongebob_2);
+
+        mPhotoshopImage1 = (ImageView) rootView.findViewById(R.id.photoshop_1);
+        mPhotoshopImage2 = (ImageView) rootView.findViewById(R.id.photoshop_2);
+
+        mJoshImage1 = (ImageView) rootView.findViewById(R.id.josh_1);
+
+        mOverwatchImage1 = (ImageView) rootView.findViewById(R.id.overwatch_1);
+        mOverwatchImage2 = (ImageView) rootView.findViewById(R.id.overwatch_2);
+
+        mFortniteImage1 = (ImageView) rootView.findViewById(R.id.fortnite_1);
+        mFortniteImage2 = (ImageView) rootView.findViewById(R.id.fortnite_2);
+
+        mDestinyImage1 = (ImageView) rootView.findViewById(R.id.destiny_1);
+        mDestinyImage2 = (ImageView) rootView.findViewById(R.id.destiny_2);
+
+
+
+
+
+
 
 
         return rootView;
