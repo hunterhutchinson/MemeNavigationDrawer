8521dfe8f1352536156ad900b14b23fa4d6c4103
20eskjev <20eskjev@mcps.k12.mt.us>
2018-05-29 10:32:43 -0600

    added url
    https://developer.android.com/training/basics/fragments/fragment-ui

--- app/src/main/java/org/pltw/example/memetracker/ActivityMain.java
+++ app/src/main/java/org/pltw/example/memetracker/ActivityMain.java
@@ -17,6 +17,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//https://developer.android.com/training/basics/fragments/fragment-ui
+
 public class ActivityMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
